165021b32452cca8e1640253d9c960908ec98264
shihab-2021 <shihab77023@gmail.com>
2022-01-28 08:52:46 +0600

    delete operation has updated

--- index.js
+++ index.js
@@ -139,7 +139,7 @@ async function run() {
     });
 
     // for delete blog
-    app.delete("/blog/:id", async (req, res) => {
+    app.delete("/blogs/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await blogsCollection.deleteOne(query);
